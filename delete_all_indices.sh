f8e03b05778ad3acae9eefb7c7cb3c417cbdcc2a
mammamiiiya <batsal.nepal.01@gmail.com>
2020-05-14 14:10:42 +0545

    Update delete_all_indices.sh

--- delete_all_indices.sh
+++ delete_all_indices.sh
@@ -9,15 +9,15 @@ COMMAND=$(curl -s -XGET http://127.0.0.1:9200/_cat/indices?pretty -u $EU:$EP)
 
 echo "$COMMAND" | while read LINES
 do
-  SELECTED=$(echo $LINES | awk '{print $3}' | grep -v "^[.]" | grep 20[0-9][0-9] | awk -F'-' '{print$(NF-0)}' | sed 's/\.//g')
+  SELECTED=$(echo $LINES | awk '{print $3}' | grep 20[0-9][0-9] | awk -F'-' '{print$(NF-0)}' | sed 's/\.//g')
   echo "$SELECTED" | while read EMPTY_LINES
   do
   [ -z "$EMPTY_LINES" ] && continue
   if [ "$EMPTY_LINES" -lt "$DA" ]
   then
     DEL=$(echo $LINES | awk '{print $3}')
-    #echo "127.0.0.1:9200/$DELETE"
-    curl -s -XDELETE "http://127.0.0.1:9200/$DEL" -u $EU:$EP
+    echo "DELETING $DEL"
+    curl -s -XDELETE "http://127.0.0.1:9200/$DEL" -u $EU:$EP > /dev/null
   fi
   done
 done
